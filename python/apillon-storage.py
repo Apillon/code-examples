726efb6fc5691b04734ffcb1c9955682323c0caa
Damjan Dimitrov <damjan.dimitrov@apillon.io>
2024-01-30 08:00:46 +0100

    Python script cleanup

--- python/apillon-storage.py
+++ python/apillon-storage.py
@@ -4,35 +4,34 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-def get_headers():
-    return {
-        "Authorization": f"Basic {os.getenv('API_KEY')}",
-        "Content-Type": "application/json"
-    }
-
+base_url = "https://api.apillon.io/storage/buckets"
+headers = {
+    "Authorization": f"Basic {os.getenv('API_KEY')}",
+    "Content-Type": "application/json"
+}
 
 def pretty_print(response):
     print(json.dumps(response.json()['data'], indent=4))
 
 
-def list_buckets(headers):
+def list_buckets():
     print("Listing Buckets...")
-    response = requests.get("https://api.apillon.io/storage/buckets", headers=headers)
+    response = requests.get(base_url, headers=headers)
     pretty_print(response)
 
 
-def create_new_bucket(headers):
+def create_new_bucket():
     data = {
         "name": "My Bucket",
         "description": "Bucket for storing images"
     }
-    response = requests.post("https://api.apillon.io/storage/buckets", headers=headers, json=data)
+    response = requests.post(f"{base_url}", headers=headers, json=data)
     bucket_uuid = response.json()["data"]["bucketUuid"]
     print(f"New bucket with UUID {bucket_uuid} created successfully!")
     return bucket_uuid
 
 
-def upload_to_bucket(headers, bucket_uuid, file_name, file_path):
+def upload_to_bucket(bucket_uuid, file_name, file_path):
     data = {
         "files": [{
           "fileName": file_name,
@@ -40,7 +39,7 @@ def upload_to_bucket(headers, bucket_uuid, file_name, file_path):
         }]
     }
     print("Uploading file...")
-    response = requests.post(f"https://api.apillon.io/storage/buckets/{bucket_uuid}/upload", headers=headers, json=data)
+    response = requests.post(f"{base_url}/{bucket_uuid}/upload", headers=headers, json=data)
     upload_response_json = response.json()['data']
 
     # Find upload URL for the file corresponding to the upload file's name
@@ -52,20 +51,19 @@ def upload_to_bucket(headers, bucket_uuid, file_name, file_path):
         requests.put(file_url, data=file_content)
 
     # End upload session
-    requests.post(f"https://api.apillon.io/storage/buckets/{bucket_uuid}/upload/{upload_response_json['sessionUuid']}/end", headers=headers)
+    requests.post(f"{base_url}/{bucket_uuid}/upload/{upload_response_json['sessionUuid']}/end", headers=headers)
     print("File uploaded successfully!")
 
 
-def list_bucket_content(headers, bucket_uuid):
-    response = requests.get(f"https://api.apillon.io/storage/buckets/{bucket_uuid}/content", headers=headers)
+def list_bucket_content(bucket_uuid):
+    response = requests.get(f"{base_url}/{bucket_uuid}/content", headers=headers)
     pretty_print(response)
 
 
-headers = get_headers()
-bucket_uuid = create_new_bucket(headers)
-list_buckets(headers)
+bucket_uuid = create_new_bucket()
+list_buckets()
 
 file_path = os.path.join(os.path.dirname(__file__), 'file-to-upload.txt')
-upload_to_bucket(headers, bucket_uuid, "file.txt", file_path)
+upload_to_bucket(bucket_uuid, "file.txt", file_path)
 
-list_bucket_content(headers, bucket_uuid)
+list_bucket_content(bucket_uuid)
